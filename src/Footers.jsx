27da277b5e8939685ac1c0da37fb5dd7d1d8ecf2
Bamidele joshua olayinka <Olayinka775@gmail.com>
2023-04-28 14:58:05 +0100

    Run build10

--- src/Footers.jsx
+++ src/Footers.jsx
@@ -4,34 +4,37 @@ const Footers = () => {
   return (
     <>
         {/* Text cards */}
-      <section className='mt-5'>
-          <div className='container'>
+      <section className='mt-5 p-2'>
+          <div className='container-fluid border rounded p-3'>
             <div className='row'>
-                <div className='col-sm-12 col-md-6 col-lg-3 mx-auto text-end'>
+                <div className='col-sm-12 col-md-6 col-lg-3 mx-auto text-center mt-2'>
                     <b>Products</b>
                     <p className='mt-3'><a href="" className='nav-link links'>Airtime to cash</a></p>
                     <p className='mt-2'><a href="" className='nav-link links'>Naira Pin</a></p>
                     <p className='mt-2'><a href="" className='nav-link links'>Data Top-up</a></p>
                     <p className='mt-2'><a href="" className='nav-link links'>Data Top-up</a></p>
+                    <hr />
                 </div>
 
-                <div className='col-sm-12 col-md-6 col-lg-3 mx-auto text-end'>
+                <div className='col-sm-12 col-md-6 col-lg-3 mx-auto text-center mt-2'>
                     <b>Company</b>
                     <p className='mt-3'><a href="" className='nav-link links'>About</a></p>
                     <p className='mt-2'><a href="" className='nav-link links'>Our Blog</a></p>
                     <p className='mt-2'><a href="" className='nav-link links'>Privacy policy</a></p>
                     <p className='mt-2'><a href="" className='nav-link links'>Term of Service</a></p>
+                    <hr />
                 </div>
 
-                <div className='col-sm-12 col-md-6 col-lg-3 mx-auto text-end'>
+                <div className='col-sm-12 col-md-6 col-lg-3 mx-auto text-center mt-2'>
                     <b>Developers</b>
                     <p className='mt-3'><a href="" className='nav-link links'>API Reference</a></p>
                     <p className='mt-2'><a href="" className='nav-link links'>Documentation</a></p>
                     <p className='mt-2'><a href="" className='nav-link links'>Libraries</a></p>
-                    {/* <p className='mt-2'><a href="" className='nav-link links'>Term of Service</a></p> */}
+                    <p className='mt-2 '><a href="" className='nav-link links'></a></p>
+                    <hr />
                 </div>
 
-                <div className='col-sm-12 col-md-6 col-lg-3 mx-auto text-end'>
+                <div className='col-sm-12 col-md-6 col-lg-3 mx-auto text-center'>
                     <b>Social Media</b>
                     <p className='mt-3'><a href="" className='nav-link links'>API Reference</a></p>
                     <p className='mt-2'><a href="" className='nav-link links'>Documentation</a></p>
@@ -48,6 +51,7 @@ const Footers = () => {
           <div className='container-fluid'>
               <div className='text-center p-4'>
                   <div className='last p-4'>&copy;2023 . Rusham Technology Ltd.</div>
+              <div className='text-end'>JOSH - BAM</div>
               </div>
           </div>
       </section>
