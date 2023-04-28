a4ae17ac225cf9d9edb44eb2669074d1efdca49b
Bamidele joshua olayinka <Olayinka775@gmail.com>
2023-04-28 12:58:23 +0100

    Run buil6

--- src/Cards2.jsx
+++ src/Cards2.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import image from './assets/time.PNG'
 
 const Cards2 = () => {
   return (
@@ -8,7 +9,7 @@ const Cards2 = () => {
       <section className='mt-5 pt-4'>
           <div className='container-fluid'>
             <div className='row mx-auto text-center'>
-                <div className=''><img src="" alt="" /></div>
+                <div className=''><img src={image} alt="image" /></div>
                 <div>
                     <b>Fast & Reliable.</b>
                     <p>With experience, we have learnt in the past years to be able to fully optimize our platform for speed and dependability.</p>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import menu from '../assets/men.PNG'
 
 const Navbar = () => {
   return (
     <>
-      
+
     </>
   );
 }
