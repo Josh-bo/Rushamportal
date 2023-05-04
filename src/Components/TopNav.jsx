9ffd26b95cd0f215b4227359ca021fa7b5416c96
Bamidele joshua olayinka <Olayinka775@gmail.com>
2023-05-04 19:39:40 +0100

    finished with the top bar again2

--- src/Components/TopNav.jsx
+++ src/Components/TopNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './assets/Capture-removebg-preview (1).PNG'
-
+import rush from '../assets/rushlogo.PNG'
+// import sup from '../assets/sup.PNG'
 
 const TopNav = () => {
   return (
@@ -9,7 +9,7 @@ const TopNav = () => {
       <div className='text-center'>
           <div class="dropdown">
             <div className='' type="button" data-bs-toggle="dropdown" aria-expanded="false">
-              <img src={logo} alt="" width={"300px"} />
+              <img src={rush} alt="" width={"300px"} />
             </div>
     
       <ul class="dropdown-menu down text-center w-100 bg-">
