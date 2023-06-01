015fe40c43cdff3368cf6ff5b65f85db3552c40e
Bamidele joshua olayinka <Olayinka775@gmail.com>
2023-06-01 23:06:30 +0100

    balance place

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -14,9 +14,16 @@ const Dashboard = () => {
 
             <div className='mt-3 text-light text-center mx-auto'>
               <b className='me-5 pe-5'>Welcome</b>
+
             </div>
             
           </nav>
+
+          <div className='container-fluid'>
+              <div className='balD mt-3 rounded-2 p-3 text-light'>
+                <p>BALANCE</p>  
+              </div>
+          </div>
         </div>
       </>
     </>
