48cca1951986cf5f24ffabf9f9ac96ea6c0828df
Bamidele joshua olayinka <Olayinka775@gmail.com>
2023-06-14 12:42:12 +0100

    I succeded in doing the dropdown menu

--- src/Components/SideScroll.jsx
+++ src/Components/SideScroll.jsx
@@ -3,12 +3,19 @@ import rush from "../assets/rushlogo.png";
 import { Link } from "react-router-dom";
 
 const SideScroll = () => {
-  const BuyAirtime = () => {
-    alert("hI")
+  const toggleDropdown = () => {
+    var dropdownContent = document.getElementById("dropdown-content");
+    dropdownContent.classList.toggle("show");
   }
 
+  const toggleDropdownForBuyData = () => {
+    var dropdownContentBuyData = document.getElementById("dropdown-contentBuyData");
+    dropdownContentBuyData.classList.toggle("show");
+  }
+
+
   return (
-    <>  
+    <>
       <div>
         <div
           class="btn border-0 "
@@ -70,23 +77,25 @@ const SideScroll = () => {
                   </div>
                 </Link>
 
-                <div className="mt-2" onClick={BuyAirtime}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-bar-chart"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M4 11H2v3h2v-3zm5-4H7v7h2V7zm5-5v12h-2V2h2zm-2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zM6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm-5 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3z" />
-                  </svg>
-                  <span className="mx-2 list">Buy Airtime</span>
+              {/* BUY AIRTIME */}
+                <div class="dropdown mt-3">
+                  <div class="dropdown" onClick={toggleDropdown}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bar-chart" viewBox="0 0 16 16">
+                      <path d="M4 11H2v3h2v-3zm5-4H7v7h2V7zm5-5v12h-2V2h2zm-2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zM6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm-5 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3z" />
+                    </svg>
+                    <span class="mx-2 list">Buy Airtime</span>
+                  </div>
+                  <div id="dropdown-content" class="dropdown-content text-center rounded-4">
+                    <a href="#" className="drop-link">MTN</a>
+                    <a href="#" className="drop-link">GLO</a>
+                    <a href="#" className="drop-link">AIRTEL</a>
+                  </div>
                 </div>
 
-                <div className="mt-3">
-                  <Link to="/buydata" className="nav-link">
-                    <svg
+                {/* BUY DATA */}
+                <div class="dropdown mt-3">
+                  <div className="dropdownBuyData" onClick={toggleDropdownForBuyData}>
+                  <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="16"
                       height="16"
@@ -97,8 +106,13 @@ const SideScroll = () => {
                       <path d="M15.384 6.115a.485.485 0 0 0-.047-.736A12.444 12.444 0 0 0 8 3C5.259 3 2.723 3.882.663 5.379a.485.485 0 0 0-.048.736.518.518 0 0 0 .668.05A11.448 11.448 0 0 1 8 4c2.507 0 4.827.802 6.716 2.164.205.148.49.13.668-.049z" />
                       <path d="M13.229 8.271a.482.482 0 0 0-.063-.745A9.455 9.455 0 0 0 8 6c-1.905 0-3.68.56-5.166 1.526a.48.48 0 0 0-.063.745.525.525 0 0 0 .652.065A8.46 8.46 0 0 1 8 7a8.46 8.46 0 0 1 4.576 1.336c.206.132.48.108.653-.065zm-2.183 2.183c.226-.226.185-.605-.1-.75A6.473 6.473 0 0 0 8 9c-1.06 0-2.062.254-2.946.704-.285.145-.326.524-.1.75l.015.015c.16.16.407.19.611.09A5.478 5.478 0 0 1 8 10c.868 0 1.69.201 2.42.56.203.1.45.07.61-.091l.016-.015zM9.06 12.44c.196-.196.198-.52-.04-.66A1.99 1.99 0 0 0 8 11.5a1.99 1.99 0 0 0-1.02.28c-.238.14-.236.464-.04.66l.706.706a.5.5 0 0 0 .707 0l.707-.707z" />
                     </svg>
-                      <span className="mx-2 list">Buy Data</span>
-                  </Link>
+                    <span class="mx-2 list">Buy Data</span>
+                  </div>
+                  <div id="dropdown-contentBuyData" className="dropdown-contentBuyData text-center rounded-4">
+                    <a href="#" className="drop-link">MTN</a>
+                    <a href="#" className="drop-link">GLO</a>
+                    <a href="#" className="drop-link">AIRTEL</a>
+                  </div>
                 </div>
 
                 <div className="mt-3">
