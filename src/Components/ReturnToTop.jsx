bca8ad49831cb4aaa626baeedbecfebdb9fec9fb
Bamidele joshua olayinka <Olayinka775@gmail.com>
2023-06-14 15:24:58 +0100

    Done with return

--- src/Components/ReturnToTop.jsx
+++ src/Components/ReturnToTop.jsx
@@ -4,14 +4,13 @@ import { Link } from 'react-router-dom';
 const ReturnToTop = () => {
   return (
     <>
-    <a href="" id='Up'>
-        
-      <div className='balD text-light returnTop fixed-bottom rounded mb-4 text-center'>
-        <div className='p-1'>
-        <i class="ri-arrow-up-fill"></i>
-        </div> 
-      </div>
-    </a>
+        <a href="#Up">
+          <div className='balD text-light returnTop fixed-bottom rounded mb-4 text-center'>
+            <div className='p-1'>
+            <i class="ri-arrow-up-fill"></i>
+            </div> 
+          </div>
+        </a>
     </>
   );
 }
