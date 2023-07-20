4d95b3fb21e5c94d21e2bf9ddb9aca33ccab6cd0
Bamidele joshua olayinka <Olayinka775@gmail.com>
2023-07-20 13:35:49 +0100

    Added the link to logout and rusham img

--- src/Components/SideScroll.jsx
+++ src/Components/SideScroll.jsx
@@ -40,7 +40,9 @@ const SideScroll = () => {
         >
           <div className="offcanvas-header text-center scroll-bg">
             <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">
-              <img src={rush} alt="" width={"140%"} height={"37px"} />
+              <Link to="/home">
+                <img src={rush} alt="" width={"140%"} height={"37px"} />
+              </Link>
             </h5>
             <button
               type="button"
@@ -200,7 +202,10 @@ const SideScroll = () => {
                       <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z" />
                       <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z" />
                     </svg>
-                    <span className="mx-2 list">LOGOUT</span>
+
+                    <Link to="/create" className="" style={{textDecoration:"none", color:"white"}}>
+                      <span className="mx-2 ">LOGOUT</span>
+                    </Link>
                   </button>
                 </div>
               </div>
